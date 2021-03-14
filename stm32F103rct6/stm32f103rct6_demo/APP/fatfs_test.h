2880742db144a61a86cb2f7756df581b94035503
dysonnnn <jheartbeat@163.com>
2021-03-14 21:40:17 +0800

    update f103rct6
    更新stm32发03rct6内容

---
+++ stm32F103rct6/stm32f103rct6_demo/APP/fatfs_test.h
@@ -0,0 +1,8 @@
+#ifndef _FATFS_TEST_H
+#define _FATFS_TEST_H
+
+#include "fatfs.h"
+#include "usart.h"
+void WriteFileTest(char *path, char *c_test_buf);
+extern char * p_FR_Table[];
+#endif
---
+++ stm32F103rct6/stm32f103rct6_demo/MDK-ARM/stm32f103rct6/main.d
@@ -0,0 +1,56 @@
+stm32f103rct6\main.o: ../Src/main.c
+stm32f103rct6\main.o: ../Inc/main.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+stm32f103rct6\main.o: ../Inc/stm32f1xx_hal_conf.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xe.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Include/core_cm3.h
+stm32f103rct6\main.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Include/cmsis_version.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+stm32f103rct6\main.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h
+stm32f103rct6\main.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rtc_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h
+stm32f103rct6\main.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h
+stm32f103rct6\main.o: ../Inc/fatfs.h
+stm32f103rct6\main.o: ../Middlewares/Third_Party/FatFs/src/ff.h
+stm32f103rct6\main.o: ../Middlewares/Third_Party/FatFs/src/integer.h
+stm32f103rct6\main.o: ../Inc/ffconf.h
+stm32f103rct6\main.o: ../Middlewares/Third_Party/FatFs/src/ff_gen_drv.h
+stm32f103rct6\main.o: ../Middlewares/Third_Party/FatFs/src/diskio.h
+stm32f103rct6\main.o: ../Inc/user_diskio.h
+stm32f103rct6\main.o: ../Inc/rtc.h
+stm32f103rct6\main.o: ../Inc/spi.h
+stm32f103rct6\main.o: ../Inc/sys.h
+stm32f103rct6\main.o: ../Inc/tim.h
+stm32f103rct6\main.o: ../Inc/usart.h
+stm32f103rct6\main.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+stm32f103rct6\main.o: ../Inc/usb_device.h
+stm32f103rct6\main.o: ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
+stm32f103rct6\main.o: ../Inc/usbd_conf.h
+stm32f103rct6\main.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+stm32f103rct6\main.o: C:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+stm32f103rct6\main.o: ../Inc/gpio.h
+stm32f103rct6\main.o: ..\BSP\w25qxx.h
+stm32f103rct6\main.o: ..\APP\fatfs_test.h
